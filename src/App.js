151c19850f581d318f3a4b8ba71f5f706588be9b
Isaac Zahau <isaaclian11@gmail.com>
2021-09-14 23:59:16 -0500

    updated video to webm

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <meta charSet="utf-8" />
         <title>Interstellar - Time Elapsed on Miller's Planet</title>
       </Helmet>
-      <video src="/background-video.mov" autoPlay loop muted playsinline/>
+      <video src="/background-video.webm" autoPlay loop muted playsinline/>
       <div className="text-container">
           <h2>Time</h2>
           <h1>E L A P S E D</h1>
