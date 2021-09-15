b51daba561e2844486ac5df20c299ff25bfb3390
Isaac Zahau <isaaclian11@gmail.com>
2021-09-14 22:54:05 -0500

    Time elapsed working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Clock from './components/Clock';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <video src="/background-video.mov" autoPlay loop muted/>
+      <div className="text-container">
+          <h1>E L A P S E D</h1>
+          <Clock className="clock-container"/>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Clock.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import moment from "moment";
+
+function Clock() {
+    const [clockState, setClockState] = useState();
+
+    useEffect(() => {
+        setInterval(() => {
+            const now = moment(new Date());
+            const release = moment("2014-11-07");
+            const duration = moment.duration(now.diff(release));
+            const hourDuration = moment.duration(duration).asHours();
+            const hour = hourDuration/(61320)
+            const ms = moment.duration(hour, 'hours').asMilliseconds();
+            const result = moment.utc(ms).format("HH:mm:ss");
+            setClockState(result);
+        }, 1000)
+    }, []);
+
+    return (
+        <>
+            <h1>{clockState}</h1>
+        </>
+    )
+}
+
+export default Clock;
