b0537edde3c0b9d823c6d5748ab23fe83ec461c8
Isaac Zahau <isaaclian11@gmail.com>
2021-09-14 23:16:50 -0500

    Fixed time delay

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,19 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import moment from "moment";
 
 function Clock() {
     const [clockState, setClockState] = useState();
 
+    function calculate() {
+        const now = moment(new Date());
+        const release = moment("2014-11-07");
+        const duration = moment.duration(now.diff(release));
+        const hourDuration = moment.duration(duration).asHours();
+        const hour = hourDuration/(61320)
+        const ms = moment.duration(hour, 'hours').asMilliseconds();
+        const result = moment.utc(ms).format("HH:mm:ss");
+        setClockState(result);
+    }
+
     useEffect(() => {
+        calculate();
         setInterval(() => {
-            const now = moment(new Date());
-            const release = moment("2014-11-07");
-            const duration = moment.duration(now.diff(release));
-            const hourDuration = moment.duration(duration).asHours();
-            const hour = hourDuration/(61320)
-            const ms = moment.duration(hour, 'hours').asMilliseconds();
-            const result = moment.utc(ms).format("HH:mm:ss");
-            setClockState(result);
+            calculate();
         }, 1000)
     }, []);
 
