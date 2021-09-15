36837a16704609d47efc37b59a38644acc6bd989
Isaac Zahau <isaaclian11@gmail.com>
2021-09-14 23:48:46 -0500

    updated icon and title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Clock from './components/Clock';
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Interstellar - Time Elapsed on Miller's Planet</title>
+      </Helmet>
       <video src="/background-video.mov" autoPlay loop muted/>
       <div className="text-container">
+          <h2>Time</h2>
           <h1>E L A P S E D</h1>
           <Clock className="clock-container"/>
       </div>
